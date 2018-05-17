7448a91db781f7cda82f42d8eea196dacb79e9b8
Andres Cifuentes <aacifuentes@gmail.com>
2018-05-16 19:57:39 -0500

    first promo section set

--- favor-runner/webpack-starter-master 7/app/scripts/components/main.js
+++ favor-runner/webpack-starter-master 7/app/scripts/components/main.js
@@ -9,6 +9,16 @@ class Main extends React.Component {
           <h5>Hi, this is Dres, your Favor Runner</h5>
           <img src={dres_icon} />
         </div>
+        <div className="promo-code">
+          <p className="promo-text">
+            If this is your first time using Favor, use this code in order to
+            get your Delivery Fee waived!
+          </p>
+          <h5>EMILC8T</h5>
+          <p className="disclaimer-text">
+            (It must be the only promo code being used)
+          </p>
+        </div>
       </main>
     );
   }
