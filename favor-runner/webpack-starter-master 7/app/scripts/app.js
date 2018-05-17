676478637883f95567213e287138c4f4df8d1a03
Andres Cifuentes <aacifuentes@gmail.com>
2018-05-16 19:38:30 -0500

    intro div and img

--- favor-runner/webpack-starter-master 7/app/scripts/app.js
+++ favor-runner/webpack-starter-master 7/app/scripts/app.js
@@ -3,29 +3,40 @@ import React from "react";
 import { BrowserRouter as Router, Route, Link } from "react-router-dom";
 import { render } from "react-dom";
 import { Provider } from "react-redux";
-import AppRoot from "./components/app_root.js";
-import About from "./components/about.js";
-import Contact from "./components/contact.js";
+// import AppRoot from "./components/app_root.js";
+// import About from "./components/about.js";
+// import Contact from "./components/contact.js";
+import Main from "./components/main.js";
 
-const Nav = () => (
-  <nav>
-    <li><Link to="/about">About</Link></li>
-    <li><Link to="/contact-us">Contact</Link></li>
-  </nav>
-);
+// const Nav = () => (
+//   <nav>
+//     <li><Link to="/about">About</Link></li>
+//     <li><Link to="/contact-us">Contact</Link></li>
+//   </nav>
+// );
 
 export default function app() {
   render(
     <Provider store={store}>
-      <Router>
-        <div>
-          <Nav />
-          <Route exact path="/" component={AppRoot} />
-          <Route exact path="/about" component={About} />
-          <Route exact path="/contact-us" component={Contact} />
-        </div>
-      </Router>
+      <div>
+        <Main />
+      </div>
     </Provider>,
     document.getElementById("app")
   );
 }
+// export default function app() {
+//   render(
+//     <Provider store={store}>
+//       <Router>
+//         <div>
+//           <Nav />
+//           <Route exact path="/" component={AppRoot} />
+//           <Route exact path="/about" component={About} />
+//           <Route exact path="/contact-us" component={Contact} />
+//         </div>
+//       </Router>
+//     </Provider>,
+//     document.getElementById("app")
+//   );
+// }
---
+++ favor-runner/webpack-starter-master 7/app/scripts/components/main.js
@@ -0,0 +1,15 @@
+import React from "react";
+
+class Main extends React.Component {
+  render() {
+    return (
+      <main>
+        <div className="intro">
+          <h5>Hi, this is Dres, your Favor Runner</h5>
+        </div>
+      </main>
+    );
+  }
+}
+
+export default Main;
