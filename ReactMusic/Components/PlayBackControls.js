a1c6018483f4b6a001e3d73815f348642b3bd61f
Andres Cifuentes <aacifuentes@gmail.com>
2018-05-30 11:17:46 -0500

    controls comp set

---
+++ ReactMusic/Components/PlayBackControls.js
@@ -0,0 +1,55 @@
+import { Image, TouchableOpacity, View, Text } from "react-native";
+import styles from "./styles";
+
+const PlayBackControls = ({
+  paused,
+  shuffleOn,
+  repeatOn,
+  onPressPlay,
+  onPressPause,
+  onBack,
+  onForward,
+  onPressShuffle,
+  onPressRepeat,
+  forwardDisabled
+}) => (
+  <View style={styles.controls_container}>
+    <TouchableOpacity activeOpacity={0} onPress={onPressShuffle}>
+      <Image
+        style={[
+          styles.controls_secondaryControl,
+          shuffleOn ? [] : styles.controls_off
+        ]}
+        source={require("../img/shuffle.png")}
+      />
+    </TouchableOpacity>
+    <View style={{ width: 40 }} />
+    {!paused ? (
+      <TouchableOpacity onPress={onPressPause}>
+        <View style={styles.controls_playButton}>
+          <Image source={require("../img/pause.png")} />
+        </View>
+      </TouchableOpacity>
+    ) : (
+      <TouchableOpacity onPress={onPressPlay}>
+        <View style={styles.controls_playButton}>
+          <Image source={require("../img/play.png")} />
+        </View>
+      </TouchableOpacity>
+    )}
+    <View style={{ width: 20 }} />
+    <TouchableOpacity onPress={onForward} disabled={forwardDisabled}>
+      <Image
+        style={[forwardDisabled && { opacity: 0.3 }]}
+        source={require("../img/next.png")}
+      />
+    </TouchableOpacity>
+    <View style={{ width: 40 }} />
+    <TouchableOpacity onPress={onPressRepeat}>
+      <Image
+        style={[styles.secondaryControl, repeatOn ? [] : styles.off]}
+        source={require("../img/repeat.png")}
+      />
+    </TouchableOpacity>
+  </View>
+);
--- ReactMusic/Components/index.js
+++ ReactMusic/Components/index.js
@@ -1,13 +1,7 @@
 import Header from "./Header";
 import AlbumArt from "./AlbumArt";
-// import PlayBackControls from './PlayBackControls';
+import PlayBackControls from "./PlayBackControls";
 import SeekBar from "./SeekBar";
 import TrackDetails from "./TrackDetails";
 
-export {
-  Header,
-  AlbumArt,
-  // PlayBackControls,
-  SeekBar,
-  TrackDetails
-};
+export { Header, AlbumArt, PlayBackControls, SeekBar, TrackDetails };
